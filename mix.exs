51254cc92e45155606113b1fce4daf9453f9c7b6
Jun Sumida <jun.sumida@mixi.co.jp>
2018-08-16 16:29:37 +0900

    bump up elixir version to 1.6

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DataCrypter.Mixfile do
   def project do
     [app: :data_crypter,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.6",
      description: "AES 128/256 GCM encrypt/decrypt wrapper for Elixir.",
      package: package(),
      build_embedded: Mix.env == :prod,
@@ -29,8 +29,8 @@ defmodule DataCrypter.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:ex_doc, "~> 0.10", only: :dev},
-     {:credo, "~> 0.4", only: [:dev, :test]}]
+    [{:ex_doc, "~> 0.18.0", only: :dev, runtime: false},
+     {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false}]
   end
 
   defp package do
